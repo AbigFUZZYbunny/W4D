1222cdc891085d9b06862a45afbbb05077947b4e
AbigFUZZYbunny <cconaway9@gmail.com>
2019-05-23 11:57:18 -0700

    commit

--- lib/utils/store.dart
+++ lib/utils/store.dart
@@ -2,24 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:whats4dinner/models/recipe_item.dart';
 
 Future<bool> updateFavoriteMeal(String uid, Recipe recipe) {
-  DocumentReference favoritesReference = Firestore.instance.collection('users').document(uid).collection('favorites').document("meal");
-
+  DocumentReference favoritesReference = Firestore.instance
+      .collection('users')
+      .document(uid)
+      .collection('favorites')
+      .document(recipe.id.toString());
+  CollectionReference favoritesCollection = Firestore.instance
+      .collection('users')
+      .document(uid)
+      .collection('favorites');
   return Firestore.instance.runTransaction((Transaction tx) async {
     DocumentSnapshot postSnapshot = await tx.get(favoritesReference);
     if (postSnapshot.exists) {
-      // Extend 'favorites' if the list does not contain the recipe ID:
-      if (!postSnapshot.data['favorites'].contains(recipe.toJson())) {
-        await tx.update(favoritesReference, <String, dynamic>{
-          'favorites': FieldValue.arrayUnion([recipe.toJson()])
-        });
-        // Delete the recipe ID from 'favorites':
-      } else {
-        await tx.update(favoritesReference, <String, dynamic>{
-          'favorites': FieldValue.arrayRemove([recipe.toJson()])
-        });
-      }
-    }else{
-      return false;
+      await tx.delete(favoritesReference);
+    } else {
+      await favoritesCollection.add(recipe.toMap());
     }
   }).then((result) {
     return true;
