d6723899ab161ca4bb66619e6b94e393b3ff4fb3
AbigFUZZYbunny <cconaway9@gmail.com>
2019-06-04 17:27:57 -0700

    Update groceries.dart

--- lib/screens/groceries.dart
+++ lib/screens/groceries.dart
@@ -14,54 +14,61 @@ class GroceriesScreen extends StatefulWidget {
 
 class GroceriesScreenState extends State<GroceriesScreen> {
   final int _selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: MainColor,
-          title: Text(
-            "Groceries",
-            style: Theme.of(context).textTheme.display3,
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        backgroundColor: MainColor,
+        title: Text(
+          "Groceries",
+          style: Theme
+              .of(context)
+              .textTheme
+              .display3,
         ),
-        body: new DefaultTabController(
-          length: 3,
-          child: new Column(
-            children: <Widget>[
-              new Container(
-                constraints: BoxConstraints(maxHeight: 150.0),
-                child: new Material(
-                  color: OffWhite,
-                  child: new TabBar(
-                    labelColor: DarkGray,
-                    unselectedLabelColor: Gray,
-                    indicatorColor: SecondaryColor,
-                    tabs: _buildTabs(),
-                  ),
+        centerTitle: true,
+      ),
+      body: new DefaultTabController(
+        length: 3,
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              constraints: BoxConstraints(maxHeight: 150.0),
+              child: new Material(
+                color: OffWhite,
+                child: new TabBar(
+                  labelColor: DarkGray,
+                  unselectedLabelColor: Gray,
+                  indicatorColor: SecondaryColor,
+                  tabs: _buildTabs(),
                 ),
               ),
-              new Expanded(
-                child: _buildTabViews(context),
-              ),
-            ],
-          ),
+            ),
+            new Expanded(
+              child: _buildTabViews(context),
+            ),
+          ],
         ),
-        bottomNavigationBar: BottomMenuBar(selectedIndex: _selectedIndex,),
+      ),
+      bottomNavigationBar: BottomMenuBar(selectedIndex: _selectedIndex,),
     );
   }
-  List<Tab> _buildTabs(){
+
+  List<Tab> _buildTabs() {
     List<Tab> _tabs = new List<Tab>();
     _tabs.add(new Tab(text: "Required"));
     _tabs.add(new Tab(text: "Shopping List"));
     _tabs.add(new Tab(text: "Pantry"));
     return _tabs;
   }
-  TabBarView _buildTabViews(BuildContext context){
+
+  TabBarView _buildTabViews(BuildContext context) {
     TabBarView _ret = new TabBarView(children: <Widget>[],);
     List<IngredientItem> pantry = pantryList(context);
     List<IngredientItem> required = requiredList(context);
-    if(required.length > 0) {
+    List<IngredientItem> shopping = shoppingList(context);
+    if (required.length > 0) {
       _ret.children.add(
         ListView.builder(
           itemCount: required.length,
@@ -70,17 +77,42 @@ class GroceriesScreenState extends State<GroceriesScreen> {
           },
         ),
       );
-    }else{
+    } else {
       _ret.children.add(
         Center(
           child: Text(
             "No required ingredients found",
-            style: Theme.of(context).textTheme.subtitle,
+            style: Theme
+                .of(context)
+                .textTheme
+                .subtitle,
           ),
         ),
       );
     }
-    if(pantry.length > 0) {
+    if (shopping.length > 0) {
+      _ret.children.add(
+        ListView.builder(
+          itemCount: shopping.length,
+          itemBuilder: (context, int) {
+            return IngredientListItem(shopping[int], _favoritesChanged, true,);
+          },
+        ),
+      );
+    } else {
+      _ret.children.add(
+        Center(
+          child: Text(
+            "No pantry items found",
+            style: Theme
+                .of(context)
+                .textTheme
+                .subtitle,
+          ),
+        ),
+      );
+    }
+    if (pantry.length > 0) {
       _ret.children.add(
         ListView.builder(
           itemCount: pantry.length,
@@ -89,40 +121,63 @@ class GroceriesScreenState extends State<GroceriesScreen> {
           },
         ),
       );
-    }else{
+    } else {
       _ret.children.add(
         Center(
           child: Text(
             "No pantry items found",
-            style: Theme.of(context).textTheme.subtitle,
+            style: Theme
+                .of(context)
+                .textTheme
+                .subtitle,
           ),
         ),
       );
     }
     return _ret;
   }
-  List<IngredientItem> pantryList(BuildContext context){
+  List<IngredientItem> shoppingList(BuildContext context) {
     List<IngredientItem> _ret = new List<IngredientItem>();
-    for(var r in StateWidget.of(context).state.pantry){
+    for (var r in StateWidget
+        .of(context)
+        .state
+        .shopping) {
       _ret.add(r);
     }
     return _ret;
   }
-  List<IngredientItem> requiredList(BuildContext context){
+
+  List<IngredientItem> pantryList(BuildContext context) {
     List<IngredientItem> _ret = new List<IngredientItem>();
-    for(var r in StateWidget.of(context).state.schedule){
-      for(var i in r.extendedIngredients){
+    for (var r in StateWidget
+        .of(context)
+        .state
+        .pantry) {
+      _ret.add(r);
+    }
+    return _ret;
+  }
+  List<IngredientItem> requiredList(BuildContext context) {
+    List<IngredientItem> _ret = new List<IngredientItem>();
+    for (var r in StateWidget
+        .of(context)
+        .state
+        .schedule) {
+      for (var i in r.extendedIngredients) {
         IngredientItem _ing = _ret.firstWhere((_i) => _i.id == i.id);
-        if(_ing != null){
+        if (_ing != null) {
           _ing.amount += i.amount;
           _ret.removeWhere((_i) => _i.id == i.id);
-        }else {
+        } else {
           _ing = i;
         }
-        for(var _i in StateWidget.of(context).state.pantry){
-          if(_i.amount != null){
+        for (var _i in StateWidget
+            .of(context)
+            .state
+            .pantry) {
+          if (_i.amount != null) {
             _ing.amount -= _i.amount;
-            if(_ing.amount < 0){
+            if (_ing.amount < 0) {
               _ing.amount = 0;
             }
           }
@@ -132,10 +187,28 @@ class GroceriesScreenState extends State<GroceriesScreen> {
     }
     return _ret;
   }
-  void _favoritesChanged(IngredientItem r) async {
+  Future<bool> _favoritesChanged(IngredientItem r) async {
+    for(var _i in StateWidget.of(context).state.preferences.ingredients.favorites) {
+      if (_i == r.name) {
+        setState(() {
+          StateWidget
+              .of(context)
+              .state
+              .preferences
+              .ingredients
+              .favorites
+              .removeWhere((i) => i == r.name);
+        });
+        return false;
+      }
+      setState(() {
+        StateWidget.of(context).state.preferences.ingredients.favorites.add(r.name);
+      });
+    }
     setState(() {
-      //StateWidget.of(context).state.favorites.removeWhere((rec) => rec.id == r.id);
+      StateWidget.of(context).state.preferences.ingredients.favorites.add(r.name);
     });
     //await updateFavoriteMeal(StateWidget.of(context).state.user.uid, r);
+    return true;
   }
 }
